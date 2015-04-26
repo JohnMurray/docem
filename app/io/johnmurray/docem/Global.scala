9d47d8fc07a1def0402b5de8de526c5da1036456
John Murray <me@johnmurray.io>
2015-04-26 04:27:01 -0400

    main view, mostly done
    still missing the cool stuff like searching and what not, but the main design
    and layout is there. The links also don't work... but they exist. Looksl like
    we'll need to link at least _some_ of the versions to the project to render this
    page correctly (without JS).

--- app/io/johnmurray/docem/Global.scala
+++ app/io/johnmurray/docem/Global.scala
@@ -18,8 +18,13 @@ object Global extends GlobalSettings {
 
     // some simple setup for testing
     import io.johnmurray.docem.index._
-    Cache.project += "test" -> model.Project("test")
-    Cache.project += "fireglass" -> model.Project("fireglass")
+    Cache.project += "test" -> model.Project("Test Project", "Some test project that does stuff", "test")
+    Cache.project += "fireglass" -> model.Project("Fireglass Framework",
+      """
+        |A simple framework built on top of Play for building web-services within AppNexus. The "framework"
+        |is mainly a collection of libraries aimed toward building AppNexus web services easier by integrating
+        |with core services and conforming to company standards.
+      """.stripMargin, "fg-framework")
 
   }
 }
